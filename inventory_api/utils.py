2d2974c010840d75924250723fd2d5d573ab0e98
bdobrosielski <bdobrosielski@edu.cdv.pl>
2022-03-24 11:31:37 +0100

    add pagination class

--- inventory_api/utils.py
+++ inventory_api/utils.py
@@ -2,6 +2,7 @@ import jwt
 from datetime import datetime, timedelta
 from django.conf import settings
 from user_admin.models import CustomUser
+from rest_framework.pagination import PageNumberPagination
 
 
 def get_access_token(payload, days):
@@ -32,3 +33,29 @@ def decodeJWT(bearer):
             return CustomUser.objects.get(id=decoded["user_id"])
         except Exception:
             None
+
+
+class CustomPagination(PageNumberPagination):
+    page_size = 20
+
+
+def normalize_query(query_string, findterms=re.compile(r'"([^"]+)"|(\S+)').findall, normspace=re.compile(r'\s{2,}').sub):
+    return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)]
+
+
+def get_query(query_string, search_fields):
+    query = None  # Query to search for every search term
+    terms = normalize_query(query_string)
+    for term in terms:
+        or_query = None  # Query to search for a given term in each field
+        for field_name in search_fields:
+            q = Q(**{"%s__icontains" % field_name: term})
+            if or_query is None:
+                or_query = q
+            else:
+                or_query = or_query | q
+        if query is None:
+            query = or_query
+        else:
+            query = query & or_query
+        return query
